9d82db8dbf052f98f7250c9d9a1337b9b8c89b22
Felipe Carneiro <flipecar97@gmail.com>
2019-11-29 13:56:19 -0300

    Update app.js

--- app.js
+++ app.js
@@ -14,13 +14,15 @@ scan.on('data', data => {
     wb.xlsx.readFile(path).then(wb => {
         const rowSize = wb.getWorksheet("Planilha1").rowCount;
         const randomCellNumber = Math.floor((Math.random() * rowSize) + 1);
+	    
         const gameName = wb.getWorksheet("Planilha1").getRow(randomCellNumber).getCell(2).value;
-		const gameYear = wb.getWorksheet("Planilha1").getRow(randomCellNumber).getCell(3).value;
-	    const gamePublisher = wb.getWorksheet("Planilha1").getRow(randomCellNumber).getCell(4).value;
+	const gameYear = wb.getWorksheet("Planilha1").getRow(randomCellNumber).getCell(3).value;
+	const gamePublisher = wb.getWorksheet("Planilha1").getRow(randomCellNumber).getCell(4).value;
         const gameGenre = wb.getWorksheet("Planilha1").getRow(randomCellNumber).getCell(5).value;
+	    
         console.log(`--\nName: ${gameName}\n`);
-		console.log(`Year: ${gameYear}\n`);
-		console.log(`Publisher: ${gamePublisher}\n`);
-		console.log(`Genre: ${gameGenre}\n--`);
+	console.log(`Year: ${gameYear}\n`);
+	console.log(`Publisher: ${gamePublisher}\n`);
+	console.log(`Genre: ${gameGenre}\n--`);
     }).catch(() => console.log(`No ${excelFile}.xlsx found.`));
 });
